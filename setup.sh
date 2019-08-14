eae65825576f2c8edb4cf10cfab275b486cf9df3
sergiug <sergiu.devel@gmail.com>
2019-08-14 20:58:01 +0000

    docker, docker-compose and glances

---
+++ setup.sh
@@ -0,0 +1,25 @@
+apt-get update
+
+#install docker (remove old versions first)
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+#install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#install glances
+apt-get install -y glances
