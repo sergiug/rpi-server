0a661f9d4a7a36e08d2a29116bf5449ebce5f94c
Sergiu Gilca <sergiu.devel@gmail.com>
2019-08-20 20:08:35 +0300

    much easier install

--- setup.sh
+++ setup.sh
@@ -1,22 +1,7 @@
 apt-get update
 
-#install docker (remove old versions first)
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get -y install docker-ce docker-compose
-
-sudo usermod -aG docker $USER
+#install docker
+curl -sSL https://get.docker.com | sh
 
 #install glances
 apt-get install -y glances
