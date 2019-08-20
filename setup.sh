e03591bc4b78ca8bf1889f93fbdfcbe82073b0cc
Sergiu Gilca <sergiu.devel@gmail.com>
2019-08-20 20:04:33 +0300

    changed from ubuntu to debian

--- setup.sh
+++ setup.sh
@@ -8,18 +8,15 @@ apt-get install -y \
     curl \
     gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get -y install docker-ce docker-compose
 
-#install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER
 
 #install glances
 apt-get install -y glances
